c70d00af7e5a50006f06d0fe190b7a9dca421edc
farooq <145667687+farucq@users.noreply.github.com>
2026-01-30 08:22:07 +0530

    Delete scripts directory

--- scripts/dataset_setup.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["nnUNet_raw"] = "./data/raw"
-os.environ["nnUNet_preprocessed"] = "./data/processed"
-os.environ["nnUNet_results"] = "./outputs"
-
-print("Environment variables set successfully")
